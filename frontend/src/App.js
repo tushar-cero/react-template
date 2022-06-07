a3a50af5864ac9b097c7d9e16a5717697f49dc1d
Tushar Singh <tusharsingh.contact@gmail.com>
2022-06-07 17:05:29 +0530

    hash-router-update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // import React, { useRef, useState, useEffect } from 'react';
 // import { AnimatePresence } from "framer-motion"; // cover Routes with the tag
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home></Home>}></Route>
-				<Route exact path="/home" element={<Home></Home>}></Route>
-                <Route exact path="/error" element={<Error></Error>}></Route>
-                <Route path="*" element={<Error/>}/>
-            </Routes>
-        </Router>
+	    <Routes>
+		<Route exact path="/" element={<Home></Home>}></Route>
+		<Route exact path="/home" element={<Home></Home>}></Route>
+		<Route exact path="/error" element={<Error></Error>}></Route>
+		<Route path="*" element={<Error/>}/>
+	    </Routes>
     );
 }
 
