7101a167cc7e49885031f0be84714825ee7e1113
Tushar Singh <tusharsingh.contact@gmail.com>
2022-08-09 16:18:15 +0530

    react-18.1.0-update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import {createRoot} from 'react-dom/client';
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
+const getRoot = document.getElementById('root');
+const root = createRoot(getRoot);
 
-ReactDOM.render(
-  	<HashRouter>
-    	<App />
-  	</HashRouter>,
-  	document.getElementById('root')
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
-
-/* If you want to start measuring performance in your app, pass a function to log results (for example: reportWebVitals(console.log)) or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals */ reportWebVitals();
